7ca1ae05af245a50e2367536fb9e8808fa155392
hendisantika <hendisantika@yahoo.co.id>
2025-02-13 07:00:49 +0700

    feat: Add QueryBuilder in DatabaseService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/helidonrestapiexample/service/DatabaseService.java
+++ src/main/java/id/my/hendisantika/helidonrestapiexample/service/DatabaseService.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.helidonrestapiexample.service;
 
 import id.my.hendisantika.helidonrestapiexample.dao.CRUD;
+import id.my.hendisantika.helidonrestapiexample.dao.Query;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,36 @@ public class DatabaseService<M> implements CRUD<M> {
     public DatabaseService(Class<M> target, String entityName) {
         this.targetClass = target;
         this.entityName = entityName;
+    }
+
+    class QueryBuilder implements Query<M> {
+
+        private int page = 1;
+
+        private int limit = 10;
+
+        @Override
+        public Query<M> page(int pag) {
+            this.page = pag;
+            return this;
+        }
+
+        @Override
+        public Query<M> limit(int limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        @Override
+        public List<M> get() {
+            String query = "SELECT e FROM {e} e".replace("{e}", entityName);
+            int offset = (page - 1) * limit;
+            return (List<M>) manager.createQuery(query)
+                    .setMaxResults(limit)
+                    .setFirstResult(offset)
+                    .getResultList();
+
+        }
+
     }
 }
