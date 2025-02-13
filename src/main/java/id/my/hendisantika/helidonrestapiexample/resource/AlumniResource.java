413192a6986f16c62e25993faf066e733e921008
hendisantika <hendisantika@yahoo.co.id>
2025-02-13 07:06:22 +0700

    feat: Add insert in AlumniResource🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/helidonrestapiexample/resource/AlumniResource.java
+++ src/main/java/id/my/hendisantika/helidonrestapiexample/resource/AlumniResource.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.helidonrestapiexample.resource;
 
+import id.my.hendisantika.helidonrestapiexample.dto.AlumniPayload;
 import id.my.hendisantika.helidonrestapiexample.model.Alumni;
 import id.my.hendisantika.helidonrestapiexample.service.AlumniService;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
@@ -35,4 +40,15 @@ public class AlumniResource {
     public List<Alumni> list() {
         return this.alumniService.list().page(1).get();
     }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    @Transactional
+    public Alumni insert(@Valid AlumniPayload alu) {
+        Alumni a = new Alumni();
+        alu.enrich(a);
+        return this.alumniService.insert(a);
+    }
+
 }
